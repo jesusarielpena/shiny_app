09cad45aac144d4f54aa449e89ec156aab53f807
jesusarielpena <jesusarielpena@gmail.com>
2016-10-27 18:47:14 -0500

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,1 +1,24 @@
-# ui.RshinyUI(fluidPage(  titlePanel("Census Visualizations of the United States"),    sidebarLayout(    sidebarPanel(      helpText("This app was develped to create demographic maps with                information from the 2010 US Census clasifying by different races"),            selectInput("var",                   label = "Choose a variable to display",                  choices = c("Percent White", "Percent Black",                              "Percent Hispanic", "Percent Asian"),                  selected = "Percent White"),            sliderInput("range",                   label = "Range of interest:",                  min = 0, max = 100, value = c(0, 100))      ),        mainPanel(plotOutput("map"))  )))
+# ui.R
+
+shinyUI(fluidPage(
+  titlePanel("Census Visualizations of the United States"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("This app was develped to create demographic maps with 
+               information from the 2010 US Census clasifying by different races"),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+      ),
+    
+    mainPanel(plotOutput("map"))
+  )
+))
